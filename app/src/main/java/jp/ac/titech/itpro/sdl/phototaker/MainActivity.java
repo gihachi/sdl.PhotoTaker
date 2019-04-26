8cc4b8790812e7fa419c6e23bc7cb838ed60933c
gihachi <38201016+gihachi@users.noreply.github.com>
2019-04-26 19:46:17 +0900

    improve quality of image and retain image when display is rotate

--- app/src/main/java/jp/ac/titech/itpro/sdl/phototaker/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/phototaker/MainActivity.java
@@ -5,18 +5,26 @@ import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 import android.provider.MediaStore;
+import android.os.Environment;
+import android.net.Uri;
+import android.support.v4.content.FileProvider;
+import android.graphics.BitmapFactory;
 
 import java.util.List;
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
     private final static int REQ_PHOTO = 1234;
     private Bitmap photoImage = null;
+    private String photoPath;
+    private final String PHOTO_KEY = "MainActivity.photoPath";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +36,17 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+                File photoFilePath = new File(storageDir,"temp.jpg");
+
+                photoPath = photoFilePath.getAbsolutePath();
+
+                Uri photoURI = FileProvider.getUriForFile(MainActivity.this,
+                        "com.example.android.fileprovider",
+                        photoFilePath);
+
                 Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
 
                 PackageManager manager = getPackageManager();
                 List activities = manager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
@@ -39,6 +57,10 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        if(savedInstanceState != null){
+            photoPath = savedInstanceState.getString(PHOTO_KEY);
+        }
     }
 
     private void showPhoto() {
@@ -49,23 +71,62 @@ public class MainActivity extends AppCompatActivity {
         photoView.setImageBitmap(photoImage);
     }
 
+    private void setImageFromFile(){
+        ImageView imageView = findViewById(R.id.photo_view);
+
+        // Get the dimensions of the View
+        int targetW = imageView.getWidth();
+        int targetH = imageView.getHeight();
+
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(photoPath, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+
+        Log.d("with","targetW:"+targetW+" targetH:"+targetH);
+        // Determine how much to scale down the image
+        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+        photoImage = BitmapFactory.decodeFile(photoPath, bmOptions);
+    }
+
     @Override
     protected void onActivityResult(int reqCode, int resCode, Intent data) {
         super.onActivityResult(reqCode, resCode, data);
         switch (reqCode) {
             case REQ_PHOTO:
                 if (resCode == RESULT_OK) {
-                    Bundle extras = data.getExtras();
-                    photoImage = (Bitmap) extras.get("data");
-                    showPhoto();
+
                 }
                 break;
         }
     }
 
     @Override
+    protected void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+        outState.putString(PHOTO_KEY,photoPath);
+    }
+
+    @Override
     protected void onResume() {
         super.onResume();
         showPhoto();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus){
+        super.onWindowFocusChanged(hasFocus);
+        if(hasFocus){
+            setImageFromFile();
+            showPhoto();
+        }
     }
 }
